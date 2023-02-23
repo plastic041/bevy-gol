fe2efe309598b835208a95fb6806fcb4471ab738
plastic041 <plastic041@gmail.com>
2023-02-23 22:05:38 +0900

    verlet

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,111 @@
-use std::collections::HashMap;
-
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
+    sprite::MaterialMesh2dBundle,
 };
 
-#[derive(Component, Clone)]
-struct Cell {
-    x: i32,
-    y: i32,
-    is_alive: bool,
+#[derive(Component)]
+struct VerletObject {
+    position_current: Vec2,
+    position_previous: Vec2,
+    acceleration: Vec2,
 }
 
 #[derive(Component)]
-struct CellHashMap(HashMap<(i32, i32), bool>);
+struct Constraint;
 
-impl Cell {
-    fn new(x: i32, y: i32) -> Self {
+impl VerletObject {
+    fn new(position: Vec2) -> Self {
         Self {
-            x,
-            y,
-            is_alive: rand::random(),
+            position_current: position,
+            position_previous: position,
+            acceleration: Vec2::ZERO,
         }
+    }
+
+    fn update_position(&mut self, delta_time: f32) {
+        let position_next = self.position_current
+            + (self.position_current - self.position_previous)
+            + (self.acceleration * delta_time * delta_time);
+        self.position_previous = self.position_current;
+        self.position_current = position_next;
+        self.acceleration = Vec2::ZERO;
+    }
+
+    fn accelerate(&mut self, acceleration: Vec2) {
+        self.acceleration += acceleration;
     }
 }
 
-fn setup(mut commands: Commands) {
+const BALL_RADIUS: f32 = 25.0;
+const CONSTRAINT_POSITION: Vec2 = Vec2::new(0.0, 0.0);
+const CONSTRAINT_RADIUS: f32 = 150.0;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     // Spawn a camera
     commands.spawn(Camera2dBundle::default());
 
-    let mut cells = Vec::new();
-    for i in 0..160 {
-        for j in 0..144 {
-            cells.push(Cell::new(i, j));
-        }
-    }
-
-    for cell in cells.iter() {
-        commands.spawn((
-            SpriteBundle {
-                transform: Transform::from_xyz(
-                    cell.x as f32 * 1.0 - 80.0,
-                    cell.y as f32 * 1.0 - 72.0,
-                    0.0,
-                ),
-                sprite: Sprite {
-                    color: Color::rgb(0.0, 0.0, 0.0),
-                    custom_size: Some(Vec2::new(1.0, 1.0)),
-                    ..default()
-                },
+    // Spawn a verlet object
+    commands.spawn((
+        VerletObject::new(Vec2::new(20.0, 0.0)),
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(BALL_RADIUS).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            transform: Transform {
+                translation: Vec3::new(20.0, 0.0, 100.0),
+                scale: Vec3::new(1.0, 1.0, 1.0),
                 ..default()
             },
-            Cell {
-                x: cell.x,
-                y: cell.y,
-                is_alive: cell.is_alive,
+            ..default()
+        },
+    ));
+
+    // Spawn a constraint
+    commands.spawn((
+        Constraint,
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::Circle::new(CONSTRAINT_RADIUS).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(Color::ANTIQUE_WHITE)),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(1.0, 1.0, 1.0),
+                ..default()
             },
-        ));
-    }
-
-    let cell_hashmap: HashMap<(i32, i32), bool> = cells
-        .iter()
-        .map(|cell| ((cell.x, cell.y), cell.is_alive))
-        .collect();
-
-    commands.spawn(CellHashMap(cell_hashmap));
+            ..default()
+        },
+    ));
 }
 
-fn update_cells(
-    mut query: Query<(&mut Cell, &mut Sprite)>,
-    mut cell_hashmap_query: Query<&mut CellHashMap>,
-) {
-    let cells = &cell_hashmap_query.single().0;
-    let mut new_cells: HashMap<(i32, i32), bool> = HashMap::new();
+const GRAVITY: Vec2 = Vec2::new(0.0, -1000.0);
 
-    for (mut cell, mut sprite) in query.iter_mut() {
-        let mut alive_neighbors = 0;
-        for i in -1..=1 {
-            for j in -1..=1 {
-                if i == 0 && j == 0 {
-                    continue;
-                }
+fn update_verlet(mut query: Query<(&mut VerletObject, &mut Transform)>) {
+    for (mut verlet_object, mut transform) in query.iter_mut() {
+        // apply accelerate
+        verlet_object.accelerate(GRAVITY);
 
-                if let Some(is_neighbor_alive) = cells.get(&(cell.x + i, cell.y + j)) {
-                    if *is_neighbor_alive {
-                        alive_neighbors += 1;
-                    }
-                }
-            }
+        // apply constraint
+        let to_obj = verlet_object.position_current - CONSTRAINT_POSITION;
+        let dist = to_obj.length();
+        if dist > CONSTRAINT_RADIUS - BALL_RADIUS {
+            let n = to_obj / dist;
+            verlet_object.position_current =
+                CONSTRAINT_POSITION + n * (CONSTRAINT_RADIUS - BALL_RADIUS);
         }
 
-        let mut is_alive = false;
-        if cell.is_alive {
-            if !(2..=3).contains(&alive_neighbors) {
-                is_alive = false;
-            } else {
-                is_alive = true;
-            }
-        } else if alive_neighbors == 3 {
-            is_alive = true;
-        }
-
-        if is_alive {
-            cell.is_alive = true;
-            sprite.color = Color::rgb(1.0, 1.0, 1.0);
-            new_cells.insert((cell.x, cell.y), true);
-        } else {
-            cell.is_alive = false;
-            sprite.color = Color::rgb(0.0, 0.0, 0.0);
-            new_cells.insert((cell.x, cell.y), false);
-        }
+        // update position
+        verlet_object.update_position(1.0 / 60.0);
+        transform.translation = Vec3::new(
+            verlet_object.position_current.x,
+            verlet_object.position_current.y,
+            100.0,
+        );
     }
-
-    std::mem::swap(&mut cell_hashmap_query.single_mut().0, &mut new_cells);
 }
 
 fn main() {
@@ -131,6 +124,6 @@ fn main() {
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
-        .add_system(update_cells)
+        .add_system(update_verlet)
         .run();
 }
